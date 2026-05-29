df9ef5a75dbe4d584c5273d882810d13e7e50d15
ArthurSW06 <contato.arthursveloso@gmail.com>
2026-05-29 16:44:19 -0300

    feat(Notificavel): adiciona métodos receberAlerta e getCanaisNotificacao na interface

--- src/br/com/pysat/interfaces/Notificavel.java
+++ src/br/com/pysat/interfaces/Notificavel.java
@@ -1,4 +1,8 @@
 package br.com.pysat.interfaces;
 
+import br.com.pysat.entities.Alerta;
+
 public interface Notificavel {
+    void receberAlerta(Alerta alerta);
+    String getCanaisNotificacao();
 }
